3cbffb7092bd8bdc61f4348d02872636b8c18dc2
oliveiraduda123 <eduarda.sousa@estudante.ifro.edu.br>
2025-01-30 11:20:19 -0400

    a

---
+++ APIuf/api_libertadores/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { retornaCampeonatos } from './servico/retornaCampeonatos_servico';
+// import pool from './servico/conexao.js';
+
+const app = express();
+
+app.get('/campeonatos', async (req, res) =>{
+    const campeonatos = await retornaCampeonatos();
+    res.json(campeonatos);
+})
+
+app.listen(9000, () => {
+    const data = new Date();
+    console.log("Servidor node iniciado em: "+data);
+    // const conexao = await pool.getConnection();
+    // console.log(conexao.threadId);
+    // conexao.release();
+})
