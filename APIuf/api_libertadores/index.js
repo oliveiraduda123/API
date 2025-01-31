d44ba3f49d1e6af7db66739c5906514b599542e6
oliveiraduda123 <eduarda.sousa@estudante.ifro.edu.br>
2025-01-31 11:33:12 -0400

    a

--- APIuf/api_libertadores/index.js
+++ APIuf/api_libertadores/index.js
@@ -1,12 +1,31 @@
 import express from 'express';
-import { retornaCampeonatos, retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatos} from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatosID} from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatosTime} from './servico/retornaCampeonatos_servico.js';
+
 // import pool from './servico/conexao.js';
 
 const app = express();
 
 app.get('/campeonatos', async (req, res) =>{
-    const campeonatos = await retornaCampeonatos();
-    res.json(campeonatos);
+    let campeonatos;
+    const ano = req.query.ano;
+    const time = req.query.time;
+    if (typeof ano === 'undefined' && typeof time === 'undefined'){
+        campeonatos = await retornaCampeonatos();
+    }
+    else if (typeof ano !== 'undefined') {
+        campeonatos = await retornaCampeonatosAno(ano);
+    }
+    else if (typeof time !== 'undefined') {
+        campeonatos = await retornaCampeonatosTime(time);
+    }
+
+    if (campeonatos.length > 0 ) {
+        res.json(campeonatos);
+    } else {
+        res.status(404).json({ mensagem: "Nenhum campeonato encontrado"});
+    }
 })
 
 app.get('/campeonatos/:id', async (req, res) =>{
