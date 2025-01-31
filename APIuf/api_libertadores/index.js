6759ec6a9597b14480dd665bb987cbfd9289a3f8
oliveiraduda123 <eduarda.sousa@estudante.ifro.edu.br>
2025-01-31 11:02:09 -0400

    a

--- APIuf/api_libertadores/index.js
+++ APIuf/api_libertadores/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { retornaCampeonatos } from './servico/retornaCampeonatos_servico.js';
+import { retornaCampeonatos, retornaCampeonatosID } from './servico/retornaCampeonatos_servico.js';
 // import pool from './servico/conexao.js';
 
 const app = express();
@@ -7,6 +7,16 @@ const app = express();
 app.get('/campeonatos', async (req, res) =>{
     const campeonatos = await retornaCampeonatos();
     res.json(campeonatos);
+})
+
+app.get('/campeonatos/:id', async (req, res) =>{
+    const id = parseInt (req.params.id);
+    const campeonato = await retornaCampeonatosID(id);
+    if (campeonato.length > 0){  
+        res.json(campeonato);
+    } else {
+        res.status(404).json({ mensagem: "Nenhum campeonato encontrado"});
+    }
 })
 
 app.listen(9000, () => {
--- APIuf/api_libertadores/servico/retornaCampeonatos_servico.js
+++ APIuf/api_libertadores/servico/retornaCampeonatos_servico.js
@@ -3,9 +3,13 @@ import pool from "./conexao.js";
 export async function retornaCampeonatos() {
 const conexao = await pool.getConnection();
 const campeonatos_tb = await conexao.query('SELECT id, vice, ano FROM campeonatos');
-const campeonatos = campeonatos_tb[0];
+const campeonatos = campeonatos_tb[0];}
 
-conexao.release();
-
-return campeonatos;
+export async function  retornaCampeonatosID(id) {
+    const conexao = await pool.getConnection();
+    const campeonatos_tb = await conexao.query(
+        'SELECT, campeao, vice, ano FROM campeonatos WHERE id = +id')
+    const campeonatos = campeonatos_tb[0]
+    conexao.release();
+    return campeonatos;
 }
