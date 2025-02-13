64d9644d916cf449b914de198389084e6f3d22b2
oliveiraduda123 <eduarda.sousa@estudante.ifro.edu.br>
2025-02-13 11:25:34 -0400

    commit

--- APIuf/api_libertadores/index.js
+++ APIuf/api_libertadores/index.js
@@ -1,10 +1,51 @@
+import cors from 'cors';
 import express from 'express';
 import { retornaCampeonatos, retornaCampeonatosID, retornaCampeonatosAno, retornaCampeonatosTime } from './servico/retornaCampeonatos_servico.js';
 import { cadastroCampeonato } from './servico/cadastroCampeonato_servico.js';
+import { atualizaCampeonato, atualizaCampeonatoParcial } from './servico/atualizaCampeonatos.js';
+
 //import pool from './servico/conexao.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json()); //Suporte para JSON no corpo da requisição
+
+app.patch('/campeonatos/:id', async(req, res) => {
+    const {id} = req.params;
+    const {campeao, vice, ano} = req.body;
+
+    const camposAtualizar = {};
+    if(campeao) camposAtualizar.campeao = campeao;
+    if(vice) camposAtualizar.vice = vice;
+    if(ano) camposAtualizar.ano = ano;
+
+    if(Object.keys(camposAtualizar).length === 0){
+        res.status(400).send('Nenhum campo válido foi enviado para atualização')
+    }else{
+        const resultado = await atualizaCampeonatoParcial(id, camposAtualizar)
+        if (resultado.affectedRows > 0){
+            res.status(200).send('Registro atualizado com sucesso!');
+        }else{
+            res.status(404).send("Registro não encontrado");
+        }
+    }
+})
+
+app.put('/campeonatos/:id', async (req, res) =>{
+    const{id} = req.params;
+    const {campeao, vice, ano} = req.body
+
+    if ( campeao == undefined || vice == undefined || ano == undefined){
+        res.status(400).send('Nem todos os campos foram informados')
+    } else{
+        const resultado = await atualizaCampeonato(id, campeao, vice, ano);
+        if (resultado.affectedRows > 0){
+            res.status(200).send('Registro atualizado com sucesso!')
+        }else{
+            res.status(400).send('Registro não encontrado')
+        }
+    }
+})
 
 app.post('/campeonatos', async (req,res) => {
 const campeao = req.body.campeao;
